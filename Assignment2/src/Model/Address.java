3011a2a524f72b36d373a1a70999b0326dbbd22c
Larry Tsao <larrytsao6@gmail.com>
2026-01-18 18:24:35 -0800

    various updates when creating new assignment

---
+++ Assignment2/src/Model/Address.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author wakingstardust
+ */
+public class Address {
+    String streetName;
+    String unitNum;
+    String city;
+    String zipCode;
+    
+    public Address(){
+        
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public String getUnitNum() {
+        return unitNum;
+    }
+
+    public void setUnitNum(String unitNumber) {
+        this.unitNum = unitNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+    
+    
+    
+}
